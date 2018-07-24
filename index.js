1f1161725d6fbc6edf55626d8408d31571245e2b
Josh Sample <joshuawsample@gmail.com>
2018-07-24 19:51:28 -0400

    Bye bye

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send({hi: 'there'});
+    res.send({bye: 'bitch'});
 });
 
 
 
-
+//Dynamic port binding for deployment
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => { 
     console.log('Server is running on ' + `${PORT}`)
 });
+
+// In 'package.json' : Specify Node Environment - specify "start" script, Also: create .gitignore file
