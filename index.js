1d8b56edd8d2b2b2f035415d06cdd5b184803f39
Josh Sample <joshuawsample@gmail.com>
2018-07-26 14:35:42 -0400

    Set up OAuth using Passport

--- index.js
+++ index.js
@@ -1,16 +1,38 @@
-const express = require('express');
+const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const keys = require("./config/keys");
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({bye: 'bitch'});
-});
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback"
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log('accessToken', accessToken);
+      console.log('refreshToken', refreshToken);
+      console.log('profile', profile);
 
+    }
+  )
+);
 
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"]
+  })
+);
 
+app.get("/auth/google/callback", passport.authenticate("google"));
 //Dynamic port binding for deployment
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => { 
-    console.log('Server is running on ' + `${PORT}`)
+app.listen(PORT, () => {
+  console.log("Server is running on " + `${PORT}`);
 });
 
 // In 'package.json' : Specify Node Environment - specify "start" script, Also: create .gitignore file
