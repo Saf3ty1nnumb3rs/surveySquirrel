cdc1e641c25670afbdc6d5365a9309787c7083f8
Josh Sample <joshuawsample@gmail.com>
2018-07-28 19:08:00 -0400

    Added materialize CSS - NOT Material UI

---
+++ client/src/components/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+
+import Header from './Header';
+
+const Dashboard = () => <h2>Dashboard</h2>;
+const SurveyNew = () => <h2>SurveyNew</h2>;
+const Landing = () => <h2>Landing</h2>;
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/surveys" component={Dashboard} />
+          <Route path="/surveys/new" component={SurveyNew} />
+        </Switch>
+      </Router>
+    </div>
+  );
+};
+
+export default App;
