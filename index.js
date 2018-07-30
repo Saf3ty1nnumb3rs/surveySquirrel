0413070efb32add9293a6e3a7ffaa7fe8b052636
Josh Sample <joshuawsample@gmail.com>
2018-07-29 22:31:32 -0400

    Added base Survey model and required in index

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const bodyParser =  require('body-parser');
 const keys = require('./config/keys');
 //MUST REQUIRE MODEL BEFORE USING MODEL - ORDER MATTERS
 require('./models/User');
+require('./models/Survey')
 require('./services/passport');
 
 mongoose.connect(keys.mongoURI);
