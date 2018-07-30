604c293668706136664fea86549f18d9edffcae2
Josh Sample <joshuawsample@gmail.com>
2018-07-29 21:32:58 -0400

    Set up for deploy

--- index.js
+++ index.js
@@ -26,6 +26,15 @@ app.use(passport.session());
 require('./routes/authController')(app);
 require('./routes/billingController')(app);
 
+if(process.env.NODE_ENV === 'production'){
+    //Express will serve up production assets
+    app.use(express.static('client/build'))
+    //Express will serve index.html if it doesn't recognize the route
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
+
 //Dynamic port binding for deployment
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
