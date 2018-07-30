03aa90595523cf0d5d1fd20011a0da52b638abef
Josh Sample <joshuawsample@gmail.com>
2018-07-29 21:48:53 -0400

    Resolve server path issue

--- index.js
+++ index.js
@@ -30,6 +30,7 @@ if(process.env.NODE_ENV === 'production'){
     //Express will serve up production assets
     app.use(express.static('client/build'))
     //Express will serve index.html if it doesn't recognize the route
+    const path = require('path');
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     })
